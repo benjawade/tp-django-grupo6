a5c241377d0e01d2c5baef887353cc177675cab2
benjawade <benja.wadeg@gmail.com>
2024-06-10 15:43:41 -0300

    third commit

--- tp3/polls/urls.py
+++ tp3/polls/urls.py
@@ -20,7 +20,7 @@ from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),
+    path('home/', views.home),
     path('cargar_csv/', views.consulta, name='consulta'),
 ]
 
